37c6c1d516891b28820734cf465dc7bb993fdbf2
picleb <malvaia@inmemoria.fr>
2020-02-10 10:13:25 +0100

    Polishing

--- public/js/script.js
+++ public/js/script.js
@@ -12,7 +12,7 @@ function submitCommand(event) {
     else if ((input.value == 'hello' || input.value == 'Hello')) {
         kraken = new KrakenApi(terminal, 'websockets-results-container');
         terminal.write([
-            `Hello there ! It's a pleasure to meet you ! Let's get down to business...`,
+            `Hello there ! Let's get down to business...`,
             `Kraken being a financial service, the public requests are pretty limited (even for public data, like OHLC data) so without authentication, we won't go too far. But the point is not to go crazy here. Just connect to a server, send a message and get a reply. And write some code. About that, you can check the code on the GitHub repository. If you want to go further, you have a small code base that should be easy to understand.`,
             `Made with TypeScript, Sass and HTML5, by Picleb, with love ❤️`,
             `Type a valid command to continue. Ex: "connect" to connect to the server.`
